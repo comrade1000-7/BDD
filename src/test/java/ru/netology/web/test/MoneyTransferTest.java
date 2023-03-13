47bbbf96b4a1768af78b864f34925bf168bc36be
Vasily Parshentsev <vparshentsev@gmail.com>
2023-03-13 21:40:58 +0300

    try fix

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,22 +1,25 @@
 package ru.netology.web.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
-import ru.netology.web.page.DashboardPage;
-import ru.netology.web.page.LoginPageV1;
-import ru.netology.web.page.LoginPageV2;
-import ru.netology.web.page.LoginPageV3;
+import ru.netology.web.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.netology.web.data.DataHelper.*;
 
 class MoneyTransferTest {
+  LoginPage loginPage;
+
+  @BeforeEach
+  void setup() {
+    loginPage = open("http://localhost:9999", LoginPage.class);
+  }
 
 
   @Test
   void shouldTransferMoneyBetweenCards() {
-    var loginPage = open("http://localhost:9999", LoginPageV3.class);
     var authInfo = DataHelper.getAuthInfo();
     var verificationPage = loginPage.validLogin(authInfo);
     var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
